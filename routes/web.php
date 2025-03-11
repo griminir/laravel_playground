011e6ad259f0e6bc298dd419e28262e1f448c526
griminir <viktor.elliot.king@gmail.com>
2025-03-11 13:21:40 +0100

    restructured the jobs views, created basic job form, added routes for post and create. added @csrf to form will add validation later

--- routes/web.php
+++ routes/web.php
@@ -10,17 +10,34 @@ Route::get('/', function () {
 Route::get('/jobs', function () {
     // this is eager loading the employer to reduce the number of queries
     // to avoid the N+1 problem
-    $jobs = Job::with('employer')->paginate(3);
+    $jobs = Job::with('employer')->latest()->simplePaginate(3);
     // from least to most performance:
     // paginate -> simplePaginate(just previous next button) -> cursorPaginate(does not let you use urls like ?page=2)
 
-    return view('jobs', [
+    return view('/jobs.index', [
         'jobs' => $jobs,
     ]);
 });
 
+Route::get('/jobs/create', function () {
+    return view('jobs.create');
+});
+
 Route::get('/jobs/{id}', function ($id) {
-    return view('job', ['job' => Job::find($id)]);
+    return view('jobs.show', ['job' => Job::find($id)]);
+});
+
+Route::post('/jobs', function () {
+    // validation...
+
+    Job::create([
+        'title' => request('title'),
+        'salary' => request('salary'),
+        'employer_id' => 1,
+    ]);
+
+    return redirect('/jobs')
+        ->with('success', 'Job created successfully');
 });
 
 Route::get('/contact', function () {
