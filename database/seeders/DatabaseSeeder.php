ee12771832d7d9f965e5965418eae32e997380b1
griminir <viktor.elliot.king@gmail.com>
2025-03-11 10:20:48 +0100

    using factories to seed the database used make:seeder to make a new seeder and called it within the main seeder so it would seed the jobs also

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,8 +16,11 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
+            'first_name' => 'timmy',
+            'last_name' => 'timothy the 3rd',
             'email' => 'test@example.com',
         ]);
+
+        $this->call(JobSeeder::class);
     }
 }
