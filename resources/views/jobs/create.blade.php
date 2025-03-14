13a41f775941836c3abb7e16db557fe808372bb0
griminir <viktor.elliot.king@gmail.com>
2025-03-14 22:43:44 +0100

    extracted form label/error/input into their own components

--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -10,32 +10,32 @@
 
                 <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
                     <div class="sm:col-span-4">
-                        <label for="title" class="block text-sm/6 font-medium text-gray-900">Title</label>
+                        <x-form-label for="title">
+                            Title
+                        </x-form-label>
                         <div class="mt-2">
-                            <div class="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
-                            </div>
-                            <input type="text" name="title" id="title"
-                                   class="block min-w-0 grow py-1.5 px-3 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6"
-                                   placeholder="Shift Leader" required>
+                            <x-form-input type="text"
+                                          name="title"
+                                          id="title"
+                                          placeholder="Title"
+                                          required
+                            />
                         </div>
-                        @error('title')
-                        <p class="text-xs text-red-600 font-semibold my-3">{{ $message }}</p>
-                        @enderror
+                        <x-form-error name="title"/>
                     </div>
                 </div>
 
                 <div class="sm:col-span-4">
-                    <label for="salary" class="block text-sm/6 font-medium text-gray-900">Salary</label>
+                    <x-form-label for="salary">Salary</x-form-label>
                     <div class="mt-2">
-                        <div class="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
-                        </div>
-                        <input type="text" name="salary" id="salary"
-                               class="block min-w-0 grow py-1.5 pr-3 px-3 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6"
-                               placeholder="$50,000 per year" required>
+                        <x-form-input type="number"
+                                      name="salary"
+                                      id="salary"
+                                      placeholder="Salary"
+                                      required
+                        />
                     </div>
-                    @error('salary')
-                    <p class="text-xs text-red-600 font-semibold my-3">{{ $message }}</p>
-                    @enderror
+                    <x-form-error name="salary"/>
                 </div>
                 {{--                this is another way of doing it--}}
                 {{--                <div class="mt-10">--}}
