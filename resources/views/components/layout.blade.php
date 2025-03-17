83f91c281c503547c1a268070e6074a21904e68e
griminir <viktor.elliot.king@gmail.com>
2025-03-17 12:34:47 +0100

    removed the cdn application of tailwind and imported it with npm

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,8 +5,9 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Home Page</title>
-    <script src="https://cdn.tailwindcss.com"></script>
+    <title>Playground</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    {{--    <script src="https://cdn.tailwindcss.com"></script>--}}
 </head>
 <body class="h-full">
 <div class="min-h-full">
